b577f44abac754cddf5c32181f64003a895ac38a
LeeSangMin12 <isangmin516@gmail.com>
2023-02-15 13:39:05 +0900

    회원가입 기능 완성

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const MongoClient = require('mongodb').MongoClient;
 const axios = require('axios');
 const dotenv = require('dotenv');
 const nodemailer = require('nodemailer');
-const send_google_email = require("./js/libs/node_mailer.js");
 
 dotenv.config();  //env 파일 가져오기
 
@@ -12,8 +11,8 @@ const {
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
   GOOGLE_REDIRECT_URI,
-  NAVER_CLINET_ID,
-  NAVER_CLINET_SECRET,
+  NAVER_CLIENT_ID,
+  NAVER_CLIENT_SECRET,
 } = process.env;
 
 app.use(express.json());
@@ -24,7 +23,7 @@ app.use(cors());  //모든 cors 오픈
 
 let db;
 MongoClient.connect('mongodb+srv://tkdals0920:000920@cluster0.ehjfw8p.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true }, (err, client) => {
-  if (err) return console.log(err)
+  if (err) return console.log(err);
   db = client.db('summit');
 
   app.listen(8080, () => {
@@ -36,76 +35,97 @@ app.post("/login", async (req, res) => {
   const authorization_code = req.body.data.in1;
 
   if (req.body.data.p_nm === "google_login") {
-    google_login(authorization_code);
-    res.send({
-      in1: "성공"
+    const token_info = await get_token(authorization_code);
+    const email_address = await get_email(token_info);
+
+    db.collection("login").findOne({ email: email_address }, (err, result) => {
+      if(result !== null){
+        res.send("ok");
+      } else {
+        res.send("");
+      }
     });
+
   } else if (req.body.data.p_nm === "naver_login") {
     naver_login(authorization_code);
     res.send({
       in1: "성공"
     });
   }
-  
 });
 
 /**
- * 구글 로그인 
+ * 구글에서 사용자의 토큰 정보를 가져온다.
  */
-const google_login = async (authorization_code) => {
+const get_token = async (authorization_code) => {
   const GOOGLE_AUTH_URL =
+    `https://www.googleapis.com/oauth2/v4/token?` +
     `grant_type=authorization_code&` +
     `code=${authorization_code}&` +
     `client_id=${GOOGLE_CLIENT_ID}&` +
     `client_secret=${GOOGLE_CLIENT_SECRET}&` +
     `redirect_uri=${GOOGLE_REDIRECT_URI}&`;
 
-  const hi = await axios.post("https://www.googleapis.com/oauth2/v4/token", GOOGLE_AUTH_URL, {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-  });
+  const token_info = await axios
+    .post(GOOGLE_AUTH_URL, {
+      headers: { "content-type": "application/x-www-form-urlencoded" },
+    })
+    .then((el) => {
+      return el.data;
+    })
+    .catch((err) => {
+      console.log("err", err);
+    });
 
-  const access_token = hi.data.access_token;
-  const refresh_token = hi.data.refresh_token;
-  const expires_in = hi.data.expires_in;
-  send_google_email("tkdals0920@naver.com", access_token, refresh_token, expires_in);
-}
+  return token_info;
+};
+
+/**
+ * access token으로 사용자의 이메일 주소를 받아온다.
+ */
+const get_email = async (token_info) => {
+  const access_token = token_info.access_token;
+  const user_info_url = `https://www.googleapis.com/oauth2/v2/userinfo?access_token=${access_token}`;
+
+  const user_info = await axios
+    .get(user_info_url, {
+      headers: {
+        authorization: `Bearer ${access_token}`,
+      },
+    })
+    .then((el) => {
+      return el.data;
+    })
+    .catch((err) => {
+      console.log("err", err);
+    });
+
+  const email_address = user_info.email;
+  return email_address;
+};
+
 
 /**
  * 네이버 로그인
  */
 const naver_login = async (authorization_code) => {
-  const NAVER_AUTH_URL = 
-  `grant_type=authorization_code&` +
-  `code=${authorization_code}&` +
-  `client_id=${NAVER_CLINET_ID}&` +
-  `client_secret=${NAVER_CLINET_SECRET}&`;
+  const NAVER_AUTH_URL =
+    `grant_type=authorization_code&` +
+    `code=${authorization_code}&` +
+    `client_id=${NAVER_CLIENT_ID}&` +
+    `client_secret=${NAVER_CLIENT_SECRET}&`;
 
-  const hi = await axios.post("https://auth.worksmobile.com/oauth2/v2.0/token", NAVER_AUTH_URL, {
+  const hi = await axios.post("https://nid.naver.com/oauth2.0/token", NAVER_AUTH_URL, {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
   })
-  console.log(hi);
 
-  // const NAVER_AUTH_URL = 
-  // `grant_type=authorization_code&` +
-  // `code=${authorization_code}&` +
-  // `client_id=${NAVER_CLINET_ID}&` +
-  // `client_secret=${NAVER_CLINET_SECRET}&`;
-
-  // const hi = await axios.post("https://nid.naver.com/oauth2.0/token", NAVER_AUTH_URL, {
-  //   headers: {
-  //     'Content-Type': 'application/x-www-form-urlencoded'
-  //   },
-  // });
-
-  // const access_token = hi.data.access_token;
-  // const refresh_token = hi.data.refresh_token;
-  // const expires_in = hi.data.expires_in;
-  // send_naver_email("isangmin516@gmail.com", access_token, refresh_token, expires_in);
-}; 
+  const access_token = hi.data.access_token;
+  const refresh_token = hi.data.refresh_token;
+  const expires_in = hi.data.expires_in;
+  send_naver_email("isangmin516@gmail.com", access_token, refresh_token, expires_in);
+};
 
 
 /**
